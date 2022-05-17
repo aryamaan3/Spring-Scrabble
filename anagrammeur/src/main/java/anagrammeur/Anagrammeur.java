72d82c2b19f041929c00ac2cfe61559575e864a4
nr800857 <remyng06@gmail.com>
2022-05-17 14:30:08 +0200

    Anagrammeur init

--- anagrammeur/src/main/java/anagrammeur/Anagrammeur.java
+++ anagrammeur/src/main/java/anagrammeur/Anagrammeur.java
@@ -1,6 +1,29 @@
 package anagrammeur;
 
+import plateau.Plateau;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Anagrammeur {
+
+    ArrayList<String> dictionnaire;
+    ArrayList<String> motPossible;
+    Plateau plateau;
+
+    private ArrayList<String> getDictionnaire(){
+        //Récupère le dico
+        return null;
+    }
+
+    private ArrayList<String> getMotPossible(String lettres){
+        //Trouve les mots possible par rapport au lettres
+        return null;
+    }
+
+    private ArrayList<String> trouveMotLettre(String lettres, Plateau plateau){
+        return null;
+    }
 
 
 }
