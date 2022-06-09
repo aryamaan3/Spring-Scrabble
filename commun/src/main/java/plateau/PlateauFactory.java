e770b6f68b10fcf7c4171d7afa940715e287a6d3
Aryamaan Kunwar <aryamaan3@gmail.com>
2022-06-09 23:07:29 +0200

    #20 #21 #22 #43 #44 #45

--- commun/src/main/java/plateau/PlateauFactory.java
+++ commun/src/main/java/plateau/PlateauFactory.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PlateauFactory {
-    static final List<List<Case>> plateau = Arrays.asList(
+    static List<List<Case>> plateau = Arrays.asList(
             Arrays.asList(new Case(4), new Case(0), new Case(0), new Case(1), new Case(0), new Case(0), new Case(0), new Case(4), new Case(0), new Case(0), new Case(0), new Case(1), new Case(0), new Case(0), new Case(4)),
             Arrays.asList(new Case(0), new Case(3), new Case(0), new Case(0), new Case(0), new Case(2), new Case(0), new Case(0), new Case(0), new Case(2), new Case(0), new Case(0), new Case(0), new Case(3), new Case(0)),
             Arrays.asList(new Case(0), new Case(0), new Case(3), new Case(0), new Case(0), new Case(0), new Case(1), new Case(0), new Case(1), new Case(0), new Case(0), new Case(0), new Case(3), new Case(0), new Case(0)),
@@ -24,8 +24,8 @@ public class PlateauFactory {
 
     static List<List<Case>> creerPlateau()
     {
-        for (int i = 0; plateau.size()<i; i++){
-            for (int j= 0; plateau.get(i).size()<j; j++){
+        for (int i = 0; i < plateau.size(); i++){
+            for (int j = 0; j < plateau.size(); j++){
                 plateau.get(i).get(j).setY(i);
                 plateau.get(i).get(j).setX(j);
             }
