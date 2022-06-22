48c475b3a8fb13b41c9db04417e5ae6caa587e5c
remy_ng <59832085+nr800857@users.noreply.github.com>
2022-06-22 12:16:44 +0200

    #41 Correction getMotsPossible

--- anagrammeur/src/main/java/anagrammeur/Anagrammeur.java
+++ anagrammeur/src/main/java/anagrammeur/Anagrammeur.java
@@ -1,5 +1,6 @@
 package anagrammeur;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Component;
 import plateau.Plateau;
 
@@ -11,20 +12,17 @@ import java.util.List;
 @Component
 public class Anagrammeur {
 
-    private final ArrayList<String> dictionnaire;
+    private ArrayList<String> dictionnaire = new ArrayList<String>();
     private Plateau plateau;
 
-    public Anagrammeur() throws IOException {
-        //Instanciation du dictionnaire de mots
-        dictionnaire = new ArrayList<String>();
-        initialiseDictionnaire();
+    public Anagrammeur() {
     }
 
     /**
      * Ouvre le fichier : ./anagrammeur/liste_francais.txt
      * et place son contenu dans l'ArrayList dictionnaire
      */
-    private void initialiseDictionnaire() throws IOException {
+    public void initialiseDictionnaire() throws IOException {
         String fileName = "anagrammeur/liste_francais.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -50,8 +48,9 @@ public class Anagrammeur {
     /**
      * Recherche dans le dictionnaire les mots possibles
      */
-    public ArrayList<String> getMotPossible(String lettres){
+    public ArrayList<String> getMotPossible(String lettres) throws IOException {
         //Trouve les mots possible par rapport au lettres 1 er tour
+        this.initialiseDictionnaire();
         ArrayList<String> motsPossibles = new ArrayList<>();
         List<String> lettresSplit = new ArrayList<String>(Arrays.asList(lettres.split("")));
         for (String s : dictionnaire) {
@@ -60,6 +59,8 @@ public class Anagrammeur {
                 motsPossibles.add(s);
             }
         }
+        System.out.println("Lettres envoyés par un joueur : " + lettres);
+        System.out.println("Listes de mots possible : " + motsPossibles);
         return motsPossibles;
     }
 
